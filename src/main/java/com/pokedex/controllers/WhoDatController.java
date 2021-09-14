1d750eab00128cf07b2f1ca83e921249deea4b27
Lucas Cima <lucas_cima@lucass-air.lan>
2021-09-14 16:17:36 -0500

    Response messages based on answers

--- src/main/java/com/pokedex/controllers/WhoDatController.java
+++ src/main/java/com/pokedex/controllers/WhoDatController.java
@@ -7,7 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("pokemon")
@@ -43,7 +49,7 @@ public class WhoDatController {
         return "whoisthat/home";
     }
 
-    @RequestMapping("whoisthateasy")
+    @RequestMapping(value = "whoisthateasy", method = RequestMethod.GET)
     public String displayWhoIsThatPokemonEasy(Model model) {
         Pokemon correctPoke = randomPokemon();
         Pokemon optA = randomPokemon();
@@ -59,4 +65,42 @@ public class WhoDatController {
         return"whoisthat/easy";
     }
 
+    @RequestMapping(value = "whoisthateasy", method = RequestMethod.POST)
+    public String processWhoIsThatPokemonEasy(@RequestParam(value="sub") String sub,
+                                              @RequestParam(value="correctpoke") String correctpoke) {
+        if(sub.equalsIgnoreCase(correctpoke)){
+            return "correct";
+        }
+        else {
+            return "wrong";
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
